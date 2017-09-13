7c4e4392bdef49179197b75c7729409b44a8b80c
xueliang.cai <xueliang.cai@mljr.com>
2017-09-13 21:44:23 +0800

    配置定时任务

--- src/main/java/com/leon/blog/config/MyBatisConfig.java
+++ src/main/java/com/leon/blog/config/MyBatisConfig.java
@@ -2,60 +2,47 @@ package com.leon.blog.config;
 
 
 import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.pool.DruidDataSourceFactory;
-import com.leon.blog.mapper.BaseWriteMapper;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
- * @Description
+ * @Description 主库配置
  * @Author 蔡学亮(xueliang.cai@mljr.com)
  * @Date 2017年09月12日 14:17
  */
 @Configuration
-@MapperScan(basePackages = {"com.leon.blog.mapper"},sqlSessionFactoryRef = "sqlSessionFactory",markerInterface = BaseWriteMapper.class)
+@MapperScan(basePackages = {"com.leon.blog.mapper"},sqlSessionFactoryRef = "sqlSessionFactory")
+@PropertySource("classpath:application.properties")
 public class MyBatisConfig {
-
     private static final Logger logger = LoggerFactory.getLogger(MyBatisConfig.class);
     private static final String MAPPER_XML_LOCATION = "classpath*:mapper/**/*Mapper.xml";
+    @Value("${spring.datasource.type}")
+    private Class<? extends DataSource> dataSourceType;
+    //写库
+    private static final String WRITE_DATASOURCE_PATH = "spring.datasource";
+
 
     @Bean
     @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource(DataSourceProperties properties) throws Exception {
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("url", properties.getUrl());
-        map.put("driverClassName", properties.getDriverClassName());
-        map.put("username", properties.getUsername());
-        map.put("password", properties.getPassword());
-        map.put("initialSize", "1");
-        map.put("maxActive", "20");
-        map.put("maxWait", "60000");
-        map.put("timeBetweenEvictionRunsMillis", "60000");
-        map.put("validationQuery", "SELECT 'x'");
-        map.put("testWhileIdle", "true");
-        map.put("testOnBorrow", "false");
-        map.put("testOnReturn", "false");
-        map.put("poolPreparedStatements", "false");
-        DataSource dataSource = DruidDataSourceFactory.createDataSource(map);
-        return dataSource;
+    @ConfigurationProperties(prefix = WRITE_DATASOURCE_PATH)
+    public DataSource dataSource() throws Exception {
+        return DataSourceBuilder.create().type(dataSourceType).build();
     }
-
 
     @Bean
     @Primary
@@ -63,12 +50,12 @@ public class MyBatisConfig {
         return new DataSourceTransactionManager(dataSource);
     }
 
-
     @Bean
     @Primary
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) {
         DruidDataSource druidDataSource = (DruidDataSource) dataSource;
         System.out.println("------maxActive:"+druidDataSource.getMaxActive()+"-----------");
+        System.out.println("------maxActive:"+druidDataSource.getTimeBetweenEvictionRunsMillis()+"-----------");
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         sqlSessionFactoryBean.setDataSource(dataSource);
         try {
---
+++ src/main/java/com/leon/blog/constant/DBPoolConsts.java
@@ -0,0 +1,22 @@
+package com.leon.blog.constant;
+
+/**
+ * @Description
+ * @Author 蔡学亮(xueliang.cai@mljr.com)
+ * @Date 2017年09月13日 21:12
+ */
+public interface DBPoolConsts {
+    String URL = "url";
+    String DRIVER_CLASS_NAME = "driverClassName";
+    String USERNAME = "username";
+    String PASSWORD = "password";
+    String INITIALSIZE = "initialSize";
+    String MAX_ACTIVE = "maxActive";
+    String MAX_WAIT = "maxWait";
+    String TIME_BETWEENE_VICTIONRUN_SMILLIS = "timeBetweenEvictionRunsMillis";
+    String VALIDATION_QUERY = "validationQuery";
+    String TEST_WHILE_IDLE = "testWhileIdle";
+    String TEST_ON_BORROW = "testOnBorrow";
+    String TEST_ON_RETURN = "testOnReturn";
+    String POOL_PREPARED_STATEMENTS = "poolPreparedStatements";
+}
